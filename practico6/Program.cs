bec3d14da600ee3620cc8809a8287df3565dc2ad
Hernan Ocaranza <hernanocaranza@hotmail.com.ar>
2023-06-17 00:56:38 -0300

    Punto 2 terminado

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,29 +1,50 @@
-﻿int invertir(int num)
-{
-    char[] numArray = num.ToString().ToCharArray();
-    Array.Reverse(numArray);
-    string reverseNum = "";
-    foreach (var numero in numArray)
-    {
-        reverseNum += numero;
-    }
-    return Convert.ToInt32(reverseNum);
-}
-
-//Principal
-int num = 0;
-string sNum;
+﻿//Principal
+double num1, num2, resultado = 0;
+int op;
+string res = "1";
+bool bandera = false;
 do
 {
-    Console.WriteLine("Ingrese el numero a invertir:");
-    sNum = Console.ReadLine();
-} while (!int.TryParse(sNum, out num));
-
-if (num > 0)
-{
-    Console.WriteLine($"Su numero invertido es {invertir(num)}");
-}
-else
-{
-    Console.WriteLine("Ingrese un numero positivo");
-}
+    System.Console.WriteLine("Que operacion desea realizar?");
+    System.Console.WriteLine("1) Sumar");
+    System.Console.WriteLine("2) Restar");
+    System.Console.WriteLine("3) Multiplicar");
+    System.Console.WriteLine("4) Dividir");
+    op = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("Ingrese el primer numero:");
+    num1 = Convert.ToDouble(Console.ReadLine());
+    System.Console.WriteLine("Ingrese el segundo numero:");
+    num2 = Convert.ToDouble(Console.ReadLine());
+    bandera = true;
+    switch (op)
+    {
+        case 1:
+            resultado = num1 + num2;
+            break;
+        case 2:
+            resultado = num1 - num2;
+            break;
+        case 3:
+            resultado = num1 * num2;
+            break;
+        case 4:
+            if (num2 != 0)
+                resultado = num1 / num2;
+            else
+            {
+                System.Console.WriteLine("No se puede dividir en 0");
+                bandera = false;
+            }
+            break;
+        default:
+            bandera = false;
+            System.Console.WriteLine("Esta operacion no existe ");
+            break;
+    }
+    if (bandera)
+    {
+        System.Console.WriteLine($"El resultado de la operacion es: {resultado}");
+    }
+    System.Console.WriteLine("Desea seguir operando? (si | 1) (no | 0)");
+    res = Console.ReadLine();
+} while (res != "0");
